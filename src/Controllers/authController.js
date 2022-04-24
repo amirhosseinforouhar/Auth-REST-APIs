1aae17732103a07f6dffb78d475be3634d7198b7
amirhosseinforouhar <amirfarvahar709@gmail.com>
2022-04-24 09:42:34 -0700

    clean try catch

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -3,44 +3,35 @@ const {StatusCodes} = require("http-status-codes")
 const ConflictError = require("../Errors/conflict")
 const BadRequestError = require("../Errors/badRequest")
 const UnAuthenticatedError = require("../Errors/unAuthenticated")
+const asyncWrapper = require("./asyncWrapper")
 
-const register = async (req , res , next) => {
-    try {
-       // check email already exist 
-        const existEmail = await User.findOne({email : req.body.email})
-        if (existEmail) throw new ConflictError("Email already exist")
+const register = asyncWrapper(async (req , res , next) => {
+    // check email already exist 
+    const existEmail = await User.findOne({email : req.body.email})
+    if (existEmail) throw new ConflictError("Email already exist")
 
-        const user = await User.create({...req.body})
-        res.status(StatusCodes.CREATED).json({user})
+    const user = await User.create({...req.body})
+    res.status(StatusCodes.CREATED).json({user})
 
-   } catch (error) {
-       
-        next(error)
-   }
 
-}
+})
 
-const login = async (req , res , next) => {
+const login = asyncWrapper( async (req , res , next) => {
     const {email , password} = req.body
     
-    try {
-        if(!email || !password) throw new BadRequestError("Please provide email and password")
+    if(!email || !password) throw new BadRequestError("Please provide email and password")
 
-        const user = await User.findOne({email})
-        if (!user) throw new UnAuthenticatedError("wrong email")
+    const user = await User.findOne({email})
+    if (!user) throw new UnAuthenticatedError("wrong email")
 
-        const isPasswordCorrect = await user.comparePassword(password)
-        if(!isPasswordCorrect) throw new UnAuthenticatedError("wrong password")
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect) throw new UnAuthenticatedError("wrong password")
 
-        const token = await user.createJwt()
+    const token = await user.createJwt()
 
-        res.json({user , token})
-    } catch (error) {
+    res.json({user , token})
 
-        next(error)
-    }
-}
-
+})
 module.exports = {
     register , 
     login
